349a9c594acde65ac0a58b09e86b5b8149bb1638
Vicheslav Der <efirshey@gmail.com>
2024-09-16 19:20:30 +0300

    ID added

--- task.sql
+++ task.sql
@@ -35,16 +35,16 @@ CREATE TABLE ProductInventory (
     FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID)
 );
 
-INSERT INTO Countries (Name)
+INSERT INTO Countries (ID, Name)
     VALUES (1, "Country1"), (2, "Country2");
 
-INSERT INTO Products (Name)
+INSERT INTO Products (ID, Name)
     VALUES (1, "AwersomeProduct");
 
-INSERT INTO Warehouses (Name, Address, CountryID)
+INSERT INTO Warehouses (ID, Name, Address, CountryID)
     VALUES (1, "Warehouse-1", "City-1, Street-1", 1),
            (2, "Warehouse-2", "City-2, Street-2", 2);
 
-INSERT INTO ProductInventory (ProductID, WarehouseID, WarehouseAmount)
+INSERT INTO ProductInventory (ID, ProductID, WarehouseID, WarehouseAmount)
     VALUES (1, 1, 1, 2),
            (2, 1, 2, 5);
